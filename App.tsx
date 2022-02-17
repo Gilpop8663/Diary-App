58b6546b71693350cb92ba0950056338b72e07a4
gilpop8663 <wolfye@naver.com>
2022-02-17 14:58:04 +0900

    Add - realm, Schema,Apploading 추가

--- App.tsx
+++ App.tsx
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Realm from 'realm';
 import { NavigationContainer } from '@react-navigation/native';
 import Navigator from './src/navigator/Navigator';
+import AppLoading from 'expo-app-loading';
+
+const FeelingSchema = {
+  name: 'Feeling',
+  properties: {
+    _id: 'int',
+    emotion: 'string',
+    message: 'string',
+  },
+  primaryKey: '_id',
+};
 
 export default function App() {
+  const [ready, setReady] = useState(false);
+  const startLoading = async () => {
+    const realm = await Realm.open({
+      path: 'gilDiaryDB',
+      schema: [FeelingSchema],
+    });
+    console.log(realm);
+    return console.log('hi');
+  };
+  const onFinish = () => setReady(true);
+  if (!ready) {
+    return (
+      <AppLoading
+        onError={console.error}
+        onFinish={onFinish}
+        startAsync={startLoading}
+      />
+    );
+  }
   return (
     <NavigationContainer>
       <Navigator />
